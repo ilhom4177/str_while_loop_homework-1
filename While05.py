130141d3a8c4bb131e8c7df192f6f1c0fda96f7a
ilhom <ilhommamatqulov276@gmail.com>
2023-01-18 23:09:41 +0500

    While05.py

--- While05.py
+++ While05.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+
+    c = 0
+    while i<len(s):
+        if s[i].islower():
+            c+=1 
+
+        i=i+1
+    return c
+print(main('CodeschoolUz'))
