15187990efa7379dd8649ee95c72c4da0fd05c15
ilhom <ilhommamatqulov276@gmail.com>
2023-01-18 22:19:04 +0500

    While01.py

--- While01.py
+++ While01.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+
+    sanash = 0
+    while i<len(s):
+        if s[i].isdigit():
+            sanash+=1 
+
+        i=i+1
+    return sanash
+print(main("python 2022"))
