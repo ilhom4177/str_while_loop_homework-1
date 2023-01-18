b8fdba1bb1c5a77732adf470fdd404dcc69aefda
ilhom <ilhommamatqulov276@gmail.com>
2023-01-18 22:24:16 +0500

    While03.py

--- While03.py
+++ While03.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    counter = 0
+    i = 0
+    while i < len(s):
+        if not s[i].isdigit() and not s[i].isalpha():
+            counter += 1
+        i += 1
+    return counter
+print(main('hashtag@$'))
+
+
