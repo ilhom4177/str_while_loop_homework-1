4defe6d2c380ed9e2f8fb1bb8aeca1f983daf06c
ilhom <ilhommamatqulov276@gmail.com>
2023-01-19 10:11:03 +0500

    While07.py

--- While07.py
+++ While07.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    n=0
+    while i<len(s):
+        if int(s[i])%2==0:
+            n=n+1
+        i=i+1
+    return n
+print(main("56786543250"))
