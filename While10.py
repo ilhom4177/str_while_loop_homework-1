82db863998f606c80ce4661e6dede2c4d7991010
ilhom <ilhommamatqulov276@gmail.com>
2023-01-19 10:29:32 +0500

    While10.py

--- While10.py
+++ While10.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0 
+    c = 0
+    while i<len(s):
+        if s[i].isdigit():
+            if int(s[i])%2==1:
+                c+=int(s[i])
+        i+=1
+    return c
+print(main("98421"))
